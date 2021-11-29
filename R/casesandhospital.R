05906ca586b0b6b0f87c007b0f979fa7149ff097
George Willoughby <58173685+GWilloughby99@users.noreply.github.com>
2021-11-29 16:47:29 +0000

    Update casesandhospital.R

--- R/casesandhospital.R
+++ R/casesandhospital.R
@@ -190,6 +190,100 @@ avg_hospitalisations <- hospitalisations %>%
 
 write.csv(avg_hospitalisations, file="raw-data/average_hospital_admissions.csv")
 
+# API query for hospitalisations
+
+
+#' Extracts paginated data by requesting all of the pages
+#' and combining the results.
+#'
+#' @param filters    API filters. See the API documentations for 
+#'                   additional information.
+#'                   
+#' @param structure  Structure parameter. See the API documentations 
+#'                   for additional information.
+#'                   
+#' @return list      Comprehensive list of dictionaries containing all 
+#'                   the data for the given ``filter`` and ``structure`.`
+get_paginated_data <- function (filters, structure) {
+  
+  endpoint     <- "https://api.coronavirus.data.gov.uk/v1/data"
+  results      <- list()
+  current_page <- 1
+  
+  repeat {
+    
+    httr::GET(
+      url   = endpoint,
+      query = list(
+        filters   = paste(filters, collapse = ";"),
+        structure = jsonlite::toJSON(structure, auto_unbox = TRUE),
+        page      = current_page
+      ),
+      timeout(10)
+    ) -> response
+    
+    # Handle errors:
+    if ( response$status_code >= 400 ) {
+      err_msg = httr::http_status(response)
+      stop(err_msg)
+    } else if ( response$status_code == 204 ) {
+      break
+    }
+    
+    # Convert response from binary to JSON:
+    json_text <- content(response, "text")
+    dt        <- jsonlite::fromJSON(json_text)
+    results   <- rbind(results, dt$data)
+    
+    if ( is.null( dt$pagination$`next` ) ){
+      break
+    }
+    
+    current_page <- current_page + 1;
+    
+  }
+  
+  return(results)
+  
+}
+
+
+# Create filters:
+query_filters <- c(
+  "areaType=nation",
+  "areaName=england"
+)
+
+# Create the structure as a list or a list of lists:
+query_structure <- list(
+  date       = "date", 
+  name       = "areaName",
+  hospital_cases = "hospitalCases",
+  hospital_admissions = "newAdmissions"
+)
+
+hospitalisations <- get_paginated_data(query_filters, query_structure)
+
+list(
+  "Shape"                = dim(hospitalisations),
+  "Data (first 3 items)" = hospitalisations[0:3, 0:-1]
+) -> report
+
+print(report)
+
+hospitalisations <- hospitalisations %>%
+  mutate_all(~replace(., is.na(.), 0))
+
+
+avg_hospitalisations <- hospitalisations %>%
+  dplyr::arrange(desc(date)) %>%
+  dplyr::group_by(name) %>%
+  dplyr::mutate(hospitalisations_seven_day_average = zoo::rollmean(hospital_admissions, k = 7, align="left", fill = NA)) %>%
+  dplyr::ungroup()
+
+
+write.csv(avg_hospitalisations, file="raw-data/latest_hospitalisations.csv")
+
 # Cases by age
 
 #' Extracts paginated data by requesting all of the pages
